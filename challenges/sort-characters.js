9d65069d1c271a1c2104e234b4d1bef8b95bc481
KLisabeth <professional.translations4ppl@gmail.com>
2020-02-22 19:23:04 +0100

    sort characters

--- challenges/sort-characters.js
+++ challenges/sort-characters.js
@@ -9,7 +9,18 @@ debugger; // step through loading & testing
   to sort strings you rearrange the characters in charCode order
 */
 function sortCharacters(str) {
-
+  var arr = str.split('');
+  var tmp;
+  for(var i = 0; i < arr.length; i++){
+    for(var j = i + 1; j < arr.length; j++){
+      if(arr[i] > arr[j]){
+        tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+      }
+    }
+  }
+  return arr.join('');
 }
 
 // declare and evaluate test cases for sortCharacters
@@ -37,14 +48,15 @@ function sortCharactersHandler() {
   debugger; // step through user actions
 
   // read & process user input
-
+  const userText = prompt("enter your text")
+  
 
   // execute core logic
-
+  const result = sortCharacters(userText);
 
   // display result to user
 
-
+ alert(result);
   // log action for developer
   console.log('\n-- sortCharacters --');
   // user inputs
